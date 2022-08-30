582020f4563202bc37bd431c4099d9f5de274752
enese <espen.enes@nov.com>
2022-08-30 09:58:17 +0200

    Big refactor of getLayout.py

--- SiemensToolBox/Step7V5/getLayout.py
+++ SiemensToolBox/Step7V5/getLayout.py
@@ -1,217 +1,179 @@
 import re
 from collections import OrderedDict
+from typing import Tuple, Any
+
+from anytree import Node, RenderTree
+from dataclasses import dataclass
+
+
+@dataclass
+class Row():
+    name = None
+    path = None
+    type = None
+    comment = None
+    address = None
+
+
+class MyNode(Node):
+    separator = "."
 
 
 class Getlayout():
-    def __init__(self, parent, rows):
-        self._rowix = 0
+
+    def __init__(self, parent=None, rows=[]):
+        # _parent is where we find the folders blocklist (BlockOfflineFolder)
+        self._nodeTree = None
         self._parent = parent
-        self.layout = self.getStruct(rows)
 
-    def getStruct(self, rows, name=""):
-        def parseOutComment(row):
-            _tmpComment = re.compile(".*\/\/(.*$)").search(row)
-            if not _tmpComment:
-                return row
-            return row[:_tmpComment.start(1) - 2]
-
-        def parseOutTypeValue(_type):
-            _tmpType = re.compile(".*:=(.*)").search(_type)
-            if not _tmpType:
-                return _type
-            return _type[:_tmpType.start(1) - 2]
-
-        root = OrderedDict()
-        while self._rowix < len(rows):
-            # _row = Row(rows[self._rowix])
-
-            row = rows[self._rowix].strip()
-            if row == "VAR_TEMP": return root
-            _comment = re.compile("^.*//(.*$)").search(row)
-
-            row = parseOutComment(row)
-            _namedStruct = re.compile("^(\w+)\s:\sSTRUCT").search(row)
-            EndStruct = re.compile("^(\w+)$").search(row)
-
-            if _namedStruct:
-                struct = GetStruct(rows[self._rowix + 1:])
-                if struct:
-                    layout = Getlayout(self._parent, struct).layout
-                    root[f"{_namedStruct.group(1)}"] = OrderedDict()
-                    for key, value in layout.items():
-                        root[f"{_namedStruct.group(1)}"][key] = value
-                    self._rowix += len(struct) + 2
-                    continue
-
-            elif EndStruct:
-                struct = GetStruct(rows[self._rowix + 1:])
-                if struct:
-                    layout = Getlayout(self._parent, struct).layout
-                    for key, value in layout.items():
-                        root[key] = value
-                    self._rowix += len(struct) + 2
-                    continue
-
-            item = re.compile("(^\w+)\s+:\s+(.*)").search(row)
-            if item:
-                name = item.group(1)
-                _type = item.group(2)
-
-                FB = re.compile("^FB\s(\d+);").search(_type)
-                UDT = re.compile("^UDT\s(\d+);").search(_type)
-                SFB = re.compile("^SFB\s(\d+)").search(_type)
-                array = re.compile("^ARRAY\s+\[(\d+)\s+..\s+(\d+)\s+]\s+(OF.*)").search(_type)
-
-                if FB:
-                    layout = self._parent.getBlockLayout(f"FB{FB.group(1)}").layout
-                    root[name] = layout
-                elif UDT:
-                    layout = self._parent.getBlockLayout(f"UDT{UDT.group(1)}").layout
-                    root[name] = layout
-
-                elif SFB:
-                    layout = self._parent.getBlockLayout(f"SFB{SFB.group(1)}").layout
-                    root[name] = layout
-                elif array:
-                    start = array.group(1)
-                    stop = array.group(2)
-
-                    if len(array.group(3).replace("OF", "").strip()) > 0:
-                        _type = parseOutTypeValue(array.group(3).replace("OF", "").strip()).replace(";", "").strip()
-                    else:
-                        _type = parseOutTypeValue(rows[self._rowix + 1]).replace(";", "").strip()
-
-                    if _type.strip() == "STRUCT":
-                        struct = GetStruct(rows[self._rowix + 1:])
-                        layout = Getlayout(self._parent, struct).layout
-                        for item in range(int(start), int(stop) + 1):
-                            root[f"{name}{[item]}"] = OrderedDict()
-                            for key, value in layout.items():
-                                root[f"{name}{[item]}"][key] = value
-                        self._rowix += len(struct) + 2
-                        continue
-
-                    else:
-                        FB = re.compile("^FB\s(\d+)").search(_type)
-                        UDT = re.compile("^UDT\s(\d+)").search(_type)
-                        SFB = re.compile("^SFB\s(\d+)").search(_type)
-
-                        if FB:
-                            layout = self._parent.getBlockLayout(f"FB{UDT.group(1)}").layout
-                            for item in range(int(start), int(stop) + 1):
-                                root[f"{name}[{item}]"] = layout
-                        elif UDT:
-                            layout = self._parent.getBlockLayout(f"UDT{UDT.group(1)}").layout
-                            for item in range(int(start), int(stop) + 1):
-                                root[f"{name}[{item}]"] = layout
-                        elif SFB:
-                            layout = self._parent.getBlockLayout(f"SFB{UDT.group(1)}").layout
-                            for item in range(int(start), int(stop) + 1):
-                                root[f"{name}[{item}]"] = layout
-                        else:
-
-                            for item in range(int(start), int(stop) + 1):
-                                root[f"{name}{[item]}"] = parseOutTypeValue(_type).replace(";", "").strip()
-
-                else:
-                    root[name] = parseOutTypeValue(_type).replace(";", "").strip()
-            self._rowix += 1
-        return root
-
-    def listLayout(self, df=None, parent=None):
-        ls = []
-        if df == None:
-            df = self.layout
-        for key, value in df.items():
-            if type(value) == OrderedDict:
-                if parent:
-                    key = f"{parent}.{key}"
-                res = self.listLayout(value, key)
-                for x in res:
-                    ls.append(x)
-            else:
-                if parent:
-                    ls.append(f"{parent}.{key} : {value}")
-                else:
-                    ls.append(f"{key} : {value}")
-        return ls
+        self.root, _ = self._structToDict(rows)
 
     @property
-    def dbLayout(self):
-        rows = self.listLayout()
+    def nodeTree(self):
+        # take an nested dictionary and make a nodetree
+        if not self._nodeTree:
+            rootNode = MyNode("root")
 
-        def checkType(type: str, types: dict) -> int:
+            #construct nodetree
+            self._nodeTree = self._dictToNodeTree(self.root, rootNode)
 
-            if type.startswith('STRING'):
-                size = re.compile(".*\[(\d+)\s]").search(type).group(1)
-                return int((int(types.get('STRING')) * int(size) + 16))
+            #Concatinate DB addresses to node leaves
+            self.getPlcAdress(self._nodeTree)
+        return self._nodeTree
 
+    @nodeTree.setter
+    def nodeTree(self, value):
+        # Store nodetree as variable in class
+        self._nodeTree = value
+
+    def _structToDict(self, rows) -> tuple[OrderedDict, int]:
+        # Parse throe all elements of a block and make a nested dictionary with row elements
+        root = OrderedDict()
+        _rowix = 0
+
+        while _rowix < len(rows):
+
+            if _namedStruct := re.compile("^(\w+)\s:\sSTRUCT").search(rows[_rowix]):
+
+                # Recursive call
+                root[_namedStruct[1]], layoutLen = self._structToDict(rows[_rowix + 1:])
+                _rowix += layoutLen + 1
+
+            elif _struct := re.compile("^STRUCT").search(rows[_rowix]):
+                _rowix += 1
+
+            elif item := re.compile("(^\w+)\s+:\s+(.*)").search(rows[_rowix]):
+
+                # check if type is an externalSorce like UDT, FB, SFB
+                if ExternalSource := re.compile("^(UDT*|FB*|SFB*)+\s(\d+)").search(item[2]):
+                    root[item[1]] = self._parent.getBlockLayout(f"{ExternalSource[1]}{ExternalSource[2]}").layout
+                    _rowix += 1
+
+                # check if type is an array
+                elif array := re.compile("^ARRAY\s+\[(\d+)\s+..\s+(\d+)\s+]\s+OF\s(.*)").search(item[2]):
+                    root[item[1]] = OrderedDict()
+
+                    # Get correct type of array
+                    if len(array[3].split("//")[0]) > 0:
+                        _type = array[3].split("//")[0]
+                        _rowix += 1
+                    else:
+                        _type = rows[_rowix + 1].replace(";", "").strip()
+                        _rowix += 2
+
+                    # check if type is an struct and needs to be parsed out
+                    if _type.strip() == "STRUCT":
+                        layout, layoutLen = self._structToDict(rows[_rowix:])
+                        for x in range(int(array[1]), int(array[2]) + 1):
+                            root[item[1]][f"{x}"] = layout
+                        _rowix += layoutLen
+
+                    # check if type is an externalSorce like UDT, FB, SFB
+                    elif ExternalSource := re.compile("^(UDT*|FB*|SFB*)+\s(\d+)").search(item[2]):
+                        layout = self._parent.getBlockLayout(f"{ExternalSource[1]}{ExternalSource[2]}").layout
+                        for x in range(int(array[1]), int(array[2]) + 1):
+                            root[item[1]][f"{x}"] = layout
+
+                    # Add Type
+                    else:
+                        for x in range(int(array[1]), int(array[2]) + 1):
+                            row = Row()
+                            row.name = x
+                            row.type = _type.replace(";", "").strip()
+                            if len(item[2].split("//")) > 1:
+                                row.comment = item[2].split("//")[1].strip()
+
+                            root[item[1]][f"{x}"] = row
+
+                # Add Type
+                else:
+                    row = Row()
+                    row.name = item[1]
+                    row.type = item[2].split("//")[0].strip().replace(";", "").strip()
+                    if len(item[2].split("//")) > 1:
+                        row.comment = item[2].split("//")[1].strip()
+                    root[item[1]] = row
+                    _rowix += 1
+
+            # Return  Struct
+            elif rows[_rowix] == "END_STRUCT;" or rows[_rowix] == "END_VAR" or rows[_rowix] == "END_STRUCT ;" or rows[
+                _rowix].startswith("END_STRUCT"):
+                _rowix += 1
+                return root, _rowix
+
+            elif rows[_rowix] == "VAR_TEMP":
+                return root, _rowix
+
+            # if here, we are loosing data, add rowix with 1 so we dont go in loop
             else:
-                try:
-                    return int(types.get(type))
-                except ValueError:
-                    print(f"concatDBAdress {type}")
-                    return 0
+                _rowix += 1
 
-        outData = list()
+        return root, _rowix
+
+    def _dictToNodeTree(self, dict: OrderedDict, root):
+        for key, value in dict.items():
+            if type(value) == OrderedDict:
+                self._dictToNodeTree(value, MyNode(key, root))
+            else:
+                _node = MyNode(key, root)
+                _node.__setattr__("row_data", value)
+        return root
+
+    def getPlcAdress(self, root: MyNode):
         adress = 0
-        dirty = True
-        oldname = None
+        old = None
 
         Types = {"BOOL": 1, "BYTE": 8, "WORD": 16, "DWORD": 32, "INT": 16, "DINT": 32, "REAL": 32, "S5TIME": 16,
                  "TIME": 32,
                  "DATE": 16, "TIME_OF:DAY": 32, "CHAR": 8, "STRING": 8, "ANY": 80, "DATE_AND_TIME": 64}
 
-        ixRows = 0
-        while ixRows < len(rows):
-            row = rows[ixRows]
+        for node in root.leaves:
+            node.row_data.path = ".".join([str(_.name) for _ in node.path][1:])
 
-            item = re.compile("(^.*)\s:\s(.*)(?:\s;)*").search(row)
+            # start at even byte if struct change
+            if node.row_data.path[:-1] != old:
+                if adress % 8 != 0:
+                    adress += (8 - (adress % 8))
+                if (adress // 8) % 2 != 0:
+                    adress += 8
+                old = node.row_data.path[:-1]
 
-            pitem = re.compile("(^.*)\s:\s(.*)(?:\s;)*").search(rows[ixRows - 1])
+            # Add DB Adress to Node
+            node.row_data.address = f"{adress // 8}.{adress % 8}"
 
-            if item:
-                name = item.group(1)
-                #is there a struct change??
-                if oldname:
-                    dirty = ".".join(name.split(".")[:-1]) != ".".join(oldname.split(".")[:-1])
-                #we have a typechange into a array
-                if (item.group(2) != pitem.group(2)) and item.group(1).endswith("]") :
-                    dirty = True
-                if item.group(1).endswith("]") and not pitem.group(1).endswith("]"):
-                    dirty = True
+            # Increement adress
+            if node.row_data.type.startswith('STRING'):
+                size = re.compile(".*\[(\d+)\s]").search(node.row_data.type)[1]
+                adress += int((int(Types.get('STRING')) * int(size) + 16))
+            else:
+                adress += int(Types.get(node.row_data.type))
 
-                elif not dirty:
-                    if item.group(2) == "BYTE":
-                        if adress % 8 != 0:
-                            adress += (8 - (adress % 8))
+    @property
+    def layout(self):
+        return self.root
 
-
-                _type = item.group(2)
-
-                if _type in Types.keys() or _type.startswith("STRING"):
-                    if _type not in ["BOOL", "BYTE"] or dirty:
-                        if adress % 8 != 0:
-                            adress += (8 - (adress % 8))
-                        if (adress // 8) % 2 != 0:
-                            adress += 8
+    @property
+    def dbLayout(self):
+        return  [(node.row_data.address, node.row_data.path, node.row_data.type) for node in self.nodeTree.leaves]
 
 
-                oldname = name
-                outData.append([f"{adress // 8}.{adress % 8}", name, _type])
-                adress += checkType(_type, Types)
-                ixRows += 1
-        return outData
-
-
-def GetStruct(rows):
-    output = []
-    for row in rows:
-        _tmpComment = re.compile(".*\/\/(.*$)").search(row)
-        if _tmpComment:
-            row = row[:_tmpComment.start(1) - 2]
-
-        if row == "END_STRUCT;" or row == "END_VAR" or row == "END_STRUCT ;" or row.startswith("END_STRUCT"):
-            return output
-        output.append(row)
-    return output
