bed19f50be378bc8b527b6f8eefd89f5e5e70dde
Espen Enes <espen.enes@nov.com>
2024-01-09 20:47:46 +0100

    Optimize array handling in getLayout.py
    This update includes modifications that improve the processing of multi-dimensional arrays in the getLayout.py file. It better handles type recognition, naming, and parsing of other necessary parameters, providing more exhaustive management of layout structures.

--- SiemensToolBox/Step7V5/getLayout.py
+++ SiemensToolBox/Step7V5/getLayout.py
@@ -229,7 +229,7 @@ class Getlayout():
             if ".".join(node.row_data.path.split(".")[:-1]) != old or node.row_data.dataType not in ["BOOL", "BYTE"]:
 
 
-                if len(node.row_data.path.split(".")) > 2 and node.row_data.path.split(".")[-2].isdigit() and node.row_data.path.split(".")[-1].isdigit():
+                if len(node.row_data.path.split(".")) > 1 and node.row_data.path.split(".")[-2].isdigit() and node.row_data.path.split(".")[-1].isdigit():
                     if ".".join(node.row_data.path.split(".")[:-1]) != ".".join(old.split(".")):
                         if adress % 8 != 0:
                             adress += (8 - (adress % 8))
